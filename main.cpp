00faceca6bfe97532fb7ad1bc43f3049cc9b87e2
Pradeep KM <pradeepkm6122004@gmail.com>
2025-03-29 16:52:36 +0530

    feat: persisting in-memory data to disk and disk data to in-memory after opening

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 using namespace std;
 
 typedef enum {
@@ -23,10 +25,12 @@ typedef enum {
     STATEMENT_SELECT 
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 typedef struct {
     uint32_t id;
-    string username;
-    string email;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
 } Row;
 
 typedef struct {
@@ -44,56 +48,168 @@ const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
+const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
-    uint32_t num_rows;
+    int file_descriptor;
+    uint32_t file_length;
     void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+typedef struct {
+    uint32_t num_rows;
+    Pager* pager;
 } Table;
 
-void print_row(Row* row) {
+void print_row (Row* row) {
     cout << row->id << " " << row->username << " " << row->email << endl;
 }
 
-void serialize_row(Row* source, void* destination) {
+void serialize_row (Row* source, void* destination) {
     memcpy((char*)destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy((char*)destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy((char*)destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+    strncpy((char*)destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy((char*)destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
-void deserialize_row(void *source, Row* destination) {
+void deserialize_row (void *source, Row* destination) {
     memcpy(&(destination->id), (char*)source + ID_OFFSET, ID_SIZE);
     memcpy(&(destination->username), (char*)source + USERNAME_OFFSET, USERNAME_SIZE);
     memcpy(&(destination->email), (char*)source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = table->pages[page_num];
-    if (page == NULL) {
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
+void* get_page (Pager* pager, uint32_t page_num) {
+    if (page_num > TABLE_MAX_PAGES) {
+        cout << "Tried to fetch page number out of bounds. " << page_num << " > " <<  TABLE_MAX_PAGES << endl;
+        exit(EXIT_FAILURE);
     }
+
+    if (pager->pages[page_num] == NULL) {
+        // Cache miss. Allocate memory and load from file.
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+        // We might save a partial page at the end of the file
+        if (pager->file_length % PAGE_SIZE) {
+            num_pages = 1;
+        }
+
+        if (page_num <= num_pages) {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if (bytes_read == -1) {
+                cout << "Error reading file: " << errno << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    }
+
+    return pager->pages[page_num];
+}
+
+void* row_slot (Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = get_page(table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return (char*)page + byte_offset;
 }
 
-Table* new_table() {
-    Table* table = (Table*)malloc(sizeof(Table));
-    table->num_rows = 0;
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-        table->pages[i] = NULL;
+Pager* pager_open (const char* filename) {
+    int fd = open(filename, 
+                O_RDWR | O_CREAT, // read / write | create new if doesnt exist
+                S_IWUSR | S_IRUSR); // user write permission | user write permission
+
+    if (fd == -1) {
+        cout << "Unable to open file: " << filename << endl;
+        exit(EXIT_FAILURE);
     }
+
+    off_t file_length = lseek(fd, 0, SEEK_END);
+
+    Pager* pager = new Pager();
+    pager->file_descriptor = fd;
+    pager->file_length = file_length;
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        pager->pages[i] = NULL;
+    }
+    return pager;
+}
+
+Table* db_open (const char* filename) {
+    Pager* pager = pager_open(filename);
+    uint32_t num_rows = pager->file_length / ROW_SIZE;
+    Table* table = new Table();
+    table->pager = pager;
+    table->num_rows = num_rows;
     return table;
 }
 
-void free_table(Table* table) {
-    for (int i = 0; table->pages[i]; i++) {
-        free(table->pages[i]);
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+    if (pager->pages[page_num] == NULL) {
+        cout << "Tried to flush null page" << endl;
+        exit(EXIT_FAILURE);
     }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if (offset == -1) {
+        cout << "Error seeking: " << errno << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written = write(pager->file_descriptor, pager->pages[page_num], size);
+
+    if (bytes_written == -1) {
+        cout << "Error writing: " << errno << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void db_close(Table* table) {
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++) {
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    // There may be a partial page to write to the end of the file
+    // This should not be needed after we switch to a B-tree
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != NULL) {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1) {
+        cout << "Error closing db file." << endl;
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        void* page = pager->pages[i];
+        if (page) {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+
+    free(pager);
     free(table);
 }
 
@@ -112,7 +228,7 @@ void close_buffer (string *input_buffer) {
 MetaCommandResult do_meta_command (string *input_buffer, Table *table) {
     if (*input_buffer == "silly exit") {
         close_buffer(input_buffer);
-        free_table(table);
+        db_close(table);
         exit(EXIT_SUCCESS);
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
@@ -138,8 +254,8 @@ PrepareResult prepare_insert (string *input_buffer, Statement* statement) {
     if (id < 0) return PREPARE_NEGATIVE_ID;
 
     statement->row_to_insert.id = id;
-    statement->row_to_insert.username = username;
-    statement->row_to_insert.email = email;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
 
     return PREPARE_SUCCESS;
 }
@@ -155,24 +271,27 @@ PrepareResult prepare_statement (string *input_buffer, Statement *statement) {
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-ExecuteResult execute_insert (Statement *statement, Table *table) {
-    if (table->num_rows >= TABLE_MAX_ROWS) {
-        return EXECUTE_TABLE_FULL;
-    }
-
-    Row *row_to_insert = &(statement->row_to_insert);
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
-    table->num_rows += 1;
-    
-    return EXECUTE_SUCCESS;
-}
-
 ExecuteResult execute_select (Statement *statement, Table *table) {
     Row row;
     for (uint32_t i = 0; i < table->num_rows; i++) {
         deserialize_row(row_slot(table, i), &row);
         print_row(&row);
     }
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_insert (Statement *statement, Table *table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    // execute_select(statement, table);
+    
     return EXECUTE_SUCCESS;
 }
 
@@ -186,7 +305,15 @@ ExecuteResult execute_statement (Statement *statement, Table *table) {
 }
 
 int main (int argc, char** argv) {
-    Table* table = new_table();
+    
+    if (argc < 2) {
+        cout << "Enter a database filename" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    Table* table = db_open(filename);
+
     string *input_buffer = new string();
     while (true) {
         print_prompt();
