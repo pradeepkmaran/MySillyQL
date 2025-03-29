ca1fc8012ce90205fbb60ea34506ea59b41f3ded
Pradeep KM <pradeepkm6122004@gmail.com>
2025-03-29 17:19:06 +0530

    feat: use cursor to read rows

--- main.cpp
+++ main.cpp
@@ -64,8 +64,32 @@ typedef struct {
     Pager* pager;
 } Table;
 
+typedef struct {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table; 
+} Cursor;
+
 void print_row (Row* row) {
     cout << row->id << " " << row->username << " " << row->email << endl;
+}
+
+Cursor* table_start(Table* table) {
+    Cursor* cursor = new Cursor();
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = new Cursor;
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
 }
 
 void serialize_row (Row* source, void* destination) {
@@ -111,12 +135,19 @@ void* get_page (Pager* pager, uint32_t page_num) {
     return pager->pages[page_num];
 }
 
-void* row_slot (Table* table, uint32_t row_num) {
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void *page = get_page(table->pager, page_num);
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+void* cursor_value (Cursor *cursor) {
+    uint32_t page_num = cursor->row_num / ROWS_PER_PAGE;
+    void *page = get_page(cursor->table->pager, page_num);
+    uint32_t row_offset = cursor->row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return (char*)page + byte_offset;
+}
+
+void cursor_advance (Cursor* cursor) {
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows) {
+        cursor->end_of_table = true;
+    }
 }
 
 Pager* pager_open (const char* filename) {
@@ -272,11 +303,14 @@ PrepareResult prepare_statement (string *input_buffer, Statement *statement) {
 }
 
 ExecuteResult execute_select (Statement *statement, Table *table) {
+    Cursor* cursor = table_start(table);
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table, i), &row);
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+    free(cursor);
     return EXECUTE_SUCCESS;
 }
 
@@ -286,12 +320,11 @@ ExecuteResult execute_insert (Statement *statement, Table *table) {
     }
 
     Row *row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
 
-    // execute_select(statement, table);
-    
     return EXECUTE_SUCCESS;
 }
 
